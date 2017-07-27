7a60ff577e050420ca819e0bee7fb157c221f3cf
hale <hale.huo@zhangmen.com>
2017-07-27 18:03:28 +0800

    注解自动注册路由

--- controllers/admin-user-ctrl.go
+++ controllers/admin-user-ctrl.go
@@ -1,8 +1,8 @@
 package controllers
 
 import (
-	"fmt"
 	. "metal/models"
+	"strconv"
 	"time"
 
 	"github.com/astaxie/beego"
@@ -21,17 +21,53 @@ func (this *UserController) UserAdd() {
 func (this *UserController) Post() {
 	username := this.GetString("username") //只能接收url后面的参数，不能接收body中的参数
 	sex, _ := this.GetInt("sex")
-	createdAt := time.Now() //.Format("2006-01-02 15:04:05")
-	updatedAt := time.Now() //.Format("2006-01-02 15:04:05")
-	user := &Users{Username: username, Gender: SexMap[sex], CreatedAt: createdAt, UpdatedAt: updatedAt}
+	mobile := this.GetString("mobile")
+	email := this.GetString("email")
+	addr := this.GetString("addr")
+	createdAt := time.Now()
+	updatedAt := time.Now()
+	user := &Users{
+		Username:  username,
+		Gender:    SexMap[sex],
+		Mobile:    mobile,
+		Email:     email,
+		Addr:      addr,
+		CreatedAt: createdAt,
+		UpdatedAt: updatedAt,
+	}
 	id, err := user.AddUser()
 	if nil != err {
+		this.Data["json"] = map[string]interface{}{"msg": id}
+		this.ServeJSON()
+	} else {
 		this.Data["json"] = map[string]interface{}{"msg": err}
 		this.ServeJSON()
 	}
-	fmt.Print(id, err)
-	this.Redirect("/admin/user-list", 302)
 }
+
+/**
+ * 通过如下方式获取路由参数
+ * /admin/user/:id
+ * this.Ctx.Input.Param(":id")
+ */
+func (this *UserController) Get() {
+	idstr := this.Ctx.Input.Param(":id")
+	id, _ := strconv.Atoi(idstr)
+	user := &Users{Id: id}
+	user, err := user.FindUser()
+	if nil != err {
+		this.Data["json"] = map[string]interface{}{"result": false, "msg": this.Ctx.Input.Params()}
+	} else {
+		this.Data["json"] = map[string]interface{}{"result": true, "msg": user}
+	}
+	this.ServeJSON()
+}
+
+/**
+ * 通过如下方式获取路由参数
+ * /admin/user/get
+ *
+ */
 func (this *UserController) GetUser() {
 	username := this.Ctx.Input.Param(":username")
 	user := &Users{Username: username}
@@ -44,6 +80,11 @@ func (this *UserController) GetUser() {
 	this.ServeJSON()
 }
 
+/**
+ * 通过如下方式获取路由参数
+ * /admin/user/:id
+ * this.Ctx.Input.Param(":id")
+ */
 func (this *UserController) Put() {
 	userId, _ := this.GetInt("id")
 	username := this.GetString("username") //只能接收url后面的参数，不能接收body中的参数
@@ -57,15 +98,21 @@ func (this *UserController) Put() {
 	}
 	this.ServeJSON()
 }
+
+/**
+ * 通过如下方式获取路由参数
+ * /admin/user/:id
+ * this.Ctx.Input.Param(":id")
+ */
 func (this *UserController) UserList() {
 	user := new(Users)
 	var userPojo = []UserPOJO{}
 	userList, err := user.FindAllUser()
 	for index, u := range userList {
 		userp := new(UserPOJO)
-		userp.UserObj = u
-		userp.CreatedAtStr = u.CreatedAt.Format("2006-01-02 15:04:05")
-		userp.UpdatedAtStr = u.UpdatedAt.Format("2006-01-02 15:04:05")
+		userp.Users = u
+		userp.CreatedAt = u.CreatedAt.Format("2006-01-02 15:04:05")
+		userp.UpdatedAt = u.UpdatedAt.Format("2006-01-02 15:04:05")
 		userPojo = append(userPojo[:index], *userp)
 	}
 	if nil != err {
@@ -75,4 +122,22 @@ func (this *UserController) UserList() {
 	this.Data["userList"] = userPojo
 	this.Data["total"] = len(userPojo)
 	this.TplName = "admin/user-list.html"
+}
+
+/**
+ * 通过如下方式获取路由参数
+ * /admin/user/:id
+ * this.Ctx.Input.Param(":id")
+ */
+func (this *UserController) Delete() {
+	idstr, err := this.GetInt("userId")
+
+	user := &Users{Id: idstr}
+	id, err := user.DeleteUser()
+	if nil != err {
+		this.Data["json"] = map[string]interface{}{"result": false, "msg": err}
+	} else {
+		this.Data["json"] = map[string]interface{}{"result": true, "msg": id}
+	}
+	this.ServeJSON()
 }
---
+++ controllers/user-index-ctrl.go
@@ -0,0 +1,80 @@
+package controllers
+
+//包名并非必须和文件夹名相同，但是按照惯例最后一个路径名和包名一致
+import (
+	//	"fmt"
+	"fmt"
+	. "metal/models"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
+)
+
+// 权限验证
+var HasIndexPermission = func(ctx *context.Context) {
+	fmt.Println(">>>>>>>>>>>>>index auth")
+}
+
+type MainController struct {
+	beego.Controller
+}
+
+//用户如果没有进行注册，那么就会通过反射来执行对应的函数，如果注册了就会通过 interface 来进行执行函数
+//官方文档说可以提高性能
+// func (c *MainController) URLMapping() {
+// 	c.Mapping("Get", c.Get)
+// 	c.Mapping("MyRoute", c.MyRoute)
+// }
+
+//注解路由
+// @router / [get]
+func (c *MainController) Get() {
+	c.Data["Website"] = "http://yuedun.duapp.com"
+	c.Data["Email"] = "huo.win.n@gmail.com"
+	c.TplExt = "html"
+	//默认tpl或html后缀
+	c.TplName = "index.html"
+}
+
+// @router /test [get]
+func (c *MainController) MyRoute() {
+	c.Data["Website"] = "http://yuedun.duapp.com"
+	c.Data["Email"] = "huo.win.n@gmail.com"
+	c.Data["content"] = "这是一个自定义控制器"
+	user := &Users{}
+	userList, err := user.FindAllUser()
+	if nil != err {
+		c.Data["json"] = map[string]interface{}{"msg": err}
+		c.ServeJSON()
+	}
+	c.Data["userList"] = userList
+	c.TplName = "myroute.html" //其他数据相关的可以写到if块中，本行最好不要
+
+}
+
+// @route /getUserByName [get]
+func (this *MainController) GetUser() {
+	username := this.Ctx.Input.Param(":username")
+	user := &Users{Username: username}
+	user, err := user.FindUser()
+	if nil != err {
+		this.Data["json"] = map[string]interface{}{"result": false, "msg": this.Ctx.Input.Params()}
+	} else {
+		this.Data["json"] = map[string]interface{}{"result": true, "msg": user}
+	}
+	this.ServeJSON()
+}
+
+// @router /category [get]
+func (c *MainController) Category() {
+	c.Data["Website"] = "http://yuedun.duapp.com"
+	c.Data["Email"] = "huo.win.n@gmail.com"
+	c.TplName = "category.html"
+}
+
+// @router /about [get]
+func (c *MainController) About() {
+	c.Data["Website"] = "http://yuedun.duapp.com"
+	c.Data["Email"] = "huo.win.n@gmail.com"
+	c.TplName = "about.html"
+}
