4afcebbadda1f1222b106fe4dc2fadd8544ed407
yuedun <light_v@163.com>
2018-04-02 16:30:22 +0800

    生成密码

--- util/helper.go
+++ util/helper.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"crypto/md5"
+	"encoding/hex"
 	"fmt"
 )
 
@@ -11,7 +12,19 @@ import (
 func GetMD5(password string) string {
 	Md5Inst := md5.New()
 	Md5Inst.Write([]byte(password))
-	Result := Md5Inst.Sum([]byte(""))
-	fmt.Sprintf(">>>>>>>%x\n\n", Result)
+	Result := Md5Inst.Sum(nil)
+	// 以下两种输出结果一样
+	fmt.Printf(">>>>>>>%x\n\n", Result)
+	fmt.Println(">>>>>>>", hex.EncodeToString(Result))
 	return fmt.Sprintf("%x", Result)
+}
+
+/**
+ * 生成秘密
+ */
+func GeneratePassword(mobile string) string {
+	b := []byte(mobile)
+	p := b[7:]
+	password := "hello" + string(p)
+	return  GetMD5(password)
 }
