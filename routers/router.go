f7de6de8e64b09d650b64d6cff5b7203a53afaec
yuedun <light_v@163.com>
2018-04-13 18:34:41 +0800

    权限系统

--- routers/router.go
+++ routers/router.go
@@ -49,11 +49,12 @@ func init() {
 		beego.NSRouter("/user/delete", &controllers.UserController{}, "delete,post:DeleteUser"),
 		beego.NSRouter("/user/:id", &controllers.UserController{}, "get:UserGet"),
 		beego.NSRouter("/users", &controllers.UserController{}, "get:UserList"),
+		beego.NSRouter("/user-group", &controllers.UserGroupController{}),
 
 		//也可以使用注解自动路由
-		//beego.NSInclude(
-		//	&controllers.UserController{},
-		//),
+		beego.NSInclude(
+			&controllers.UserGroupController{},
+		),
 	)
 	//注册namespace
 	beego.AddNamespace(ns)
