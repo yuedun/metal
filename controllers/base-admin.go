a88f092f65a4468bd87b1f45d1836c1252fad440
yuedun <light_v@163.com>
2018-04-28 23:58:57 +0800

    修复未登录状态报错

--- controllers/base-admin.go
+++ controllers/base-admin.go
@@ -30,33 +30,34 @@ var HasAdminPermission = func(ctx *context.Context) {
  * 这个函数主要是为了用户扩展用的，这个函数会在下面定义的这些 Method 方法之前执行，用户可以重写这个函数实现类似用户验证之类
  */
 func (c *AdminBaseController) Prepare() {
-	// admin-user-ctrl和user-index-ctrl都继承了base-ctrl，所以都会自动执行该方法，可以做一些校验，但不适合做权限校验
+	// admin-user-ctrl和user-index-ctrl都继承了base-ctrl，所以都会自动执行该方法
 	// 因为前端用户界面不需要权限验证，管理后台才需要
 	session := c.GetSession("loginUser")
+	//session为空时不进行权限验证
 	if session != nil {
 		userPermission := session.(*UserPermission)
 		c.Data["username"] = userPermission.User.UserName
-	}
-	fmt.Println(">>>>>>>>>>>>>Prepare第二级验证是否拥有接口权限")
-	ctrl, runMethod := c.GetControllerAndAction() // 获取controller和method
-	requestPermission := ctrl + ":" + runMethod
-	fmt.Println(">>run-method:", ctrl+":"+runMethod)
-	privileges := session.(*UserPermission).Privileges
-	hasPermission := true
-	//需要权限
-	if NeedPermission[requestPermission] {
-		for _, pri := range privileges {
-			fmt.Println(pri)
-			if pri != requestPermission {
-				hasPermission = false
-			} else {
-				hasPermission = true
-				break
+		fmt.Println(">>>>>>>>>>>>>Prepare第二级验证是否拥有接口权限")
+		ctrl, runMethod := c.GetControllerAndAction() // 获取controller和method
+		requestPermission := ctrl + ":" + runMethod
+		fmt.Println(">>run-method:", ctrl+":"+runMethod)
+		privileges := session.(*UserPermission).Privileges
+		hasPermission := true
+		//需要权限
+		if NeedPermission[requestPermission] {
+			for _, pri := range privileges {
+				fmt.Println(pri)
+				if pri != requestPermission {
+					hasPermission = false
+				} else {
+					hasPermission = true
+					break
+				}
 			}
-		}
-		if !hasPermission {
-			c.Data["json"] = ErrorMsg("权限不足")
-			c.ServeJSON()
+			if !hasPermission {
+				c.Data["json"] = ErrorMsg("权限不足")
+				c.ServeJSON()
+			}
 		}
 	}
 }
