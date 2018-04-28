8758dcadf89026a2427396df6d7f2b253ea95756
hale <hale.huo@zhangmen.com>
2018-04-28 15:30:16 +0800

    权限验证完成

--- controllers/base-admin.go
+++ controllers/base-admin.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/astaxie/beego/context"
 	"metal/models"
+	"strings"
 )
 
 type AdminBaseController struct {
@@ -11,8 +12,8 @@ type AdminBaseController struct {
 }
 
 type UserPermission struct {
-	User  models.User
-	Group []models.Group
+	User       models.User
+	Privileges string //特权
 }
 
 // 后台权限验证
@@ -29,7 +30,6 @@ var HasAdminPermission = func(ctx *context.Context) {
 /**
  * 这个函数主要是为了用户扩展用的，这个函数会在下面定义的这些 Method 方法之前执行，用户可以重写这个函数实现类似用户验证之类
  */
-/*
 func (c *AdminBaseController) Prepare() {
 	// admin-user-ctrl和user-index-ctrl都继承了base-ctrl，所以都会自动执行该方法，可以做一些校验，但不适合做权限校验
 	// 因为前端用户界面不需要权限验证，管理后台才需要
@@ -39,29 +39,13 @@ func (c *AdminBaseController) Prepare() {
 		c.Data["username"] = userPermission.User.UserName
 	}
 	fmt.Println(">>>>>>>>>>>>>Prepare后端权限校验，第二级")
-	ctrl, runMethod :=c.GetControllerAndAction()// 获取controller和method
-	fmt.Println(">>run-method:", ctrl, runMethod)
+	ctrl, runMethod := c.GetControllerAndAction() // 获取controller和method
+	fmt.Println(">>run-method:", ctrl+":"+runMethod)
 	if session != nil {
-		var userGroup = new(models.UserGroup)
-		userGroupList, _ :=userGroup.GetGroupByUserId(session.(*UserPermission).User.Id)
-		hasPermission := false
-		for _, p := range userGroupList {
-			var group = new(models.Group)
-			group.Id = p.GroupId
-			err :=group.GetUserPermissions()
-			if err !=nil {
-				log.Print(err)
-			}
-			fmt.Println("group.Permissions:", group.Permissions)
-			if group.Permissions == ctrl+":"+runMethod {
-				hasPermission = true//有权限
-				break
-			}
-		}
-		if !hasPermission {
+		privileges := session.(*UserPermission).Privileges
+		if !strings.Contains(privileges, ctrl+":"+runMethod) {
 			c.Data["json"] = ErrorMsg("权限不足")
 			c.ServeJSON()
 		}
 	}
 }
-*/
