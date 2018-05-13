5985e0a1d7b01e409123bbd036b2ced9c9c5500d
yuedun <light_v@163.com>
2018-05-13 23:11:04 +0800

    通过界面添加权限

--- routers/commentsRouter_.go
+++ routers/commentsRouter_.go
@@ -9,27 +9,27 @@ func init() {
 
 	beego.GlobalControllerRouter["metal/controllers:GroupController"] = append(beego.GlobalControllerRouter["metal/controllers:GroupController"],
 		beego.ControllerComments{
-			Method:           "AddUserGroup",
-			Router:           `/user-role/add-user-role`,
-			AllowHTTPMethods: []string{"post"},
-			MethodParams:     param.Make(),
-			Params:           nil})
-
-	beego.GlobalControllerRouter["metal/controllers:GroupController"] = append(beego.GlobalControllerRouter["metal/controllers:GroupController"],
-		beego.ControllerComments{
-			Method:           "GetAllUserGroup",
-			Router:           `/user-role/get-all-user-role`,
+			Method: "GetAllRole",
+			Router: `/roles`,
 			AllowHTTPMethods: []string{"get"},
-			MethodParams:     param.Make(),
-			Params:           nil})
+			MethodParams: param.Make(),
+			Params: nil})
 
 	beego.GlobalControllerRouter["metal/controllers:GroupController"] = append(beego.GlobalControllerRouter["metal/controllers:GroupController"],
 		beego.ControllerComments{
-			Method:           "Aaa",
-			Router:           `/user/:id`,
-			AllowHTTPMethods: []string{"post", "get"},
-			MethodParams:     param.Make(),
-			Params:           nil})
+			Method: "Aaa",
+			Router: `/user/:id`,
+			AllowHTTPMethods: []string{"post","get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["metal/controllers:GroupController"] = append(beego.GlobalControllerRouter["metal/controllers:GroupController"],
+		beego.ControllerComments{
+			Method: "AddUserRole",
+			Router: `/user/groups`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
 
 	beego.GlobalControllerRouter["metal/controllers:MainController"] = append(beego.GlobalControllerRouter["metal/controllers:MainController"],
 		beego.ControllerComments{
--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -9,16 +9,8 @@ func init() {
 
 	beego.GlobalControllerRouter["metal/controllers:GroupController"] = append(beego.GlobalControllerRouter["metal/controllers:GroupController"],
 		beego.ControllerComments{
-			Method: "AddUserGroup",
-			Router: `/user-role/add-user-role`,
-			AllowHTTPMethods: []string{"post"},
-			MethodParams: param.Make(),
-			Params: nil})
-
-	beego.GlobalControllerRouter["metal/controllers:GroupController"] = append(beego.GlobalControllerRouter["metal/controllers:GroupController"],
-		beego.ControllerComments{
-			Method: "GetAllUserGroup",
-			Router: `/user-role/get-all-user-role`,
+			Method: "GetAllRole",
+			Router: `/roles`,
 			AllowHTTPMethods: []string{"get"},
 			MethodParams: param.Make(),
 			Params: nil})
@@ -28,6 +20,14 @@ func init() {
 			Method: "Aaa",
 			Router: `/user/:id`,
 			AllowHTTPMethods: []string{"post","get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["metal/controllers:GroupController"] = append(beego.GlobalControllerRouter["metal/controllers:GroupController"],
+		beego.ControllerComments{
+			Method: "AddUserRole",
+			Router: `/user/groups`,
+			AllowHTTPMethods: []string{"post"},
 			MethodParams: param.Make(),
 			Params: nil})
 
