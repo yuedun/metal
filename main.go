7d0d6766c22b366581969dc24a3cebfc547d7895
hale <hale.huo@zhangmen.com>
2018-07-07 14:09:20 +0800

    设置免登录时长

--- main.go
+++ main.go
@@ -49,6 +49,7 @@ func init() {
 	 */
 	gob.Register(&controllers.UserPermission{})
 	// session 开发环境下使用file存储，生产环境使用redis等数据库存储
+	beego.BConfig.WebConfig.Session.SessionCookieLifeTime = 60 * 60 * 24 * 3 //cookie时长
 	beego.BConfig.WebConfig.Session.SessionProvider = "file"
 	beego.BConfig.WebConfig.Session.SessionProviderConfig = "./tmp"
 
