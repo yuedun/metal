f11f60a85fda1cbb292a3308316eb78f1e3a1cce
yuedun <light_v@163.com>
2018-09-19 19:36:24 +0800

    获取真实的客户端ip地址和地理位置

--- util/helper.go
+++ util/helper.go
@@ -3,8 +3,12 @@ package util
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
+	"github.com/astaxie/beego"
+	"io/ioutil"
 	"log"
+	"net/http"
 )
 
 /**
@@ -28,4 +32,42 @@ func GeneratePassword(mobile string) string {
 	p := b[7:]
 	password := "hello" + string(p)
 	return GetMD5(password)
+}
+
+type IPBody struct {
+	Code int
+	Data struct {
+		Ip         string
+		Country    string
+		Area       string
+		Region     string
+		City       string
+		County     string
+		Isp        string
+		Country_id string
+		area_id    string
+		Region_id  string
+		City_id    string
+		County_id  string
+		Isp_id     string
+	}
+}
+
+func GetIpGeography(ip string, objBody *IPBody) {
+	ipService := beego.AppConfig.String("ipService")
+	res, err := http.Get(fmt.Sprintf(ipService, ip))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	} else {
+		log.Println("请求成功")
+		bodyByte, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		json.Unmarshal(bodyByte, &objBody)
+	}
 }
