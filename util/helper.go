49e0ce47f89fa806600623dca777e30a16845a1d
hale <hale.huo@zhangmen.com>
2019-01-17 11:00:47 +0800

    使用百度api查询地理ip地理位置

--- util/helper.go
+++ util/helper.go
@@ -35,23 +35,44 @@ func GeneratePassword(mobile string) string {
 	return GetMD5(password)
 }
 
+// 淘宝api
+// type IPBody struct {
+// 	Code int
+// 	Data struct {
+// 		Ip         string
+// 		Country    string
+// 		Area       string
+// 		Region     string
+// 		City       string
+// 		County     string
+// 		Isp        string
+// 		Country_id string
+// 		area_id    string
+// 		Region_id  string
+// 		City_id    string
+// 		County_id  string
+// 		Isp_id     string
+// 	}
+// }
+// 百度api
 type IPBody struct {
-	Code int
-	Data struct {
-		Ip         string
-		Country    string
-		Area       string
-		Region     string
-		City       string
-		County     string
-		Isp        string
-		Country_id string
-		area_id    string
-		Region_id  string
-		City_id    string
-		County_id  string
-		Isp_id     string
+	Address string
+	Content struct {
+		Address_detail struct {
+			province      string
+			City          string
+			District      string
+			Dtreet        string
+			Atreet_number string
+			City_code     int
+		}
+		Address string
+		Point   struct {
+			y float32
+			x float32
+		}
 	}
+	Status int
 }
 
 func GetIpGeography(ip string, objBody *IPBody) error {
@@ -62,7 +83,7 @@ func GetIpGeography(ip string, objBody *IPBody) error {
 	}
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
-		beego.Error("请求地理位置错误: %s", res.Status)
+		beego.Error("请求地理位置错误: ", res.Status)
 		return errors.New("请求地理位置错误：" + res.Status)
 	} else {
 		bodyByte, err := ioutil.ReadAll(res.Body)
