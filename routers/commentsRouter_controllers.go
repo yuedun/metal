50ab77d587b7c3c6e875a84dff946314c31fddff
yuedun <light_v@163.com>
2019-05-13 18:39:03 +0800

    截取字符串

--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -9,186 +9,186 @@ func init() {
 
 	beego.GlobalControllerRouter["metal/controllers:AdminController"] = append(beego.GlobalControllerRouter["metal/controllers:AdminController"],
 		beego.ControllerComments{
-			Method: "CreateArticle",
-			Router: `/article`,
+			Method:           "CreateArticle",
+			Router:           `/article`,
 			AllowHTTPMethods: []string{"post"},
-			MethodParams: param.Make(),
-			Params: nil})
+			MethodParams:     param.Make(),
+			Params:           nil})
 
 	beego.GlobalControllerRouter["metal/controllers:AdminController"] = append(beego.GlobalControllerRouter["metal/controllers:AdminController"],
 		beego.ControllerComments{
-			Method: "ArticleEditRoute",
-			Router: `/article-edit-route/:id`,
+			Method:           "ArticleEditRoute",
+			Router:           `/article-edit-route/:id`,
 			AllowHTTPMethods: []string{"get"},
-			MethodParams: param.Make(),
-			Params: nil})
+			MethodParams:     param.Make(),
+			Params:           nil})
 
 	beego.GlobalControllerRouter["metal/controllers:AdminController"] = append(beego.GlobalControllerRouter["metal/controllers:AdminController"],
 		beego.ControllerComments{
-			Method: "CreateArticleRoute",
-			Router: `/article-route`,
+			Method:           "CreateArticleRoute",
+			Router:           `/article-route`,
 			AllowHTTPMethods: []string{"get"},
-			MethodParams: param.Make(),
-			Params: nil})
+			MethodParams:     param.Make(),
+			Params:           nil})
 
 	beego.GlobalControllerRouter["metal/controllers:AdminController"] = append(beego.GlobalControllerRouter["metal/controllers:AdminController"],
 		beego.ControllerComments{
-			Method: "ArticleEdit",
-			Router: `/article/:id`,
+			Method:           "ArticleEdit",
+			Router:           `/article/:id`,
 			AllowHTTPMethods: []string{"put"},
-			MethodParams: param.Make(),
-			Params: nil})
+			MethodParams:     param.Make(),
+			Params:           nil})
 
 	beego.GlobalControllerRouter["metal/controllers:AdminController"] = append(beego.GlobalControllerRouter["metal/controllers:AdminController"],
 		beego.ControllerComments{
-			Method: "ArticleDelete",
-			Router: `/article/:id`,
+			Method:           "ArticleDelete",
+			Router:           `/article/:id`,
 			AllowHTTPMethods: []string{"delete"},
-			MethodParams: param.Make(),
-			Params: nil})
+			MethodParams:     param.Make(),
+			Params:           nil})
 
 	beego.GlobalControllerRouter["metal/controllers:AdminController"] = append(beego.GlobalControllerRouter["metal/controllers:AdminController"],
 		beego.ControllerComments{
-			Method: "ArticlesList",
-			Router: `/articles`,
+			Method:           "ArticlesList",
+			Router:           `/articles`,
 			AllowHTTPMethods: []string{"get"},
-			MethodParams: param.Make(),
-			Params: nil})
+			MethodParams:     param.Make(),
+			Params:           nil})
 
 	beego.GlobalControllerRouter["metal/controllers:AdminController"] = append(beego.GlobalControllerRouter["metal/controllers:AdminController"],
 		beego.ControllerComments{
-			Method: "ArticlesRoute",
-			Router: `/articles-route`,
+			Method:           "ArticlesRoute",
+			Router:           `/articles-route`,
 			AllowHTTPMethods: []string{"get"},
-			MethodParams: param.Make(),
-			Params: nil})
+			MethodParams:     param.Make(),
+			Params:           nil})
 
 	beego.GlobalControllerRouter["metal/controllers:AdminController"] = append(beego.GlobalControllerRouter["metal/controllers:AdminController"],
 		beego.ControllerComments{
-			Method: "GetLogsRoute",
-			Router: `/get-logs-route`,
+			Method:           "GetLogsRoute",
+			Router:           `/get-logs-route`,
 			AllowHTTPMethods: []string{"get"},
-			MethodParams: param.Make(),
-			Params: nil})
+			MethodParams:     param.Make(),
+			Params:           nil})
 
 	beego.GlobalControllerRouter["metal/controllers:AdminController"] = append(beego.GlobalControllerRouter["metal/controllers:AdminController"],
 		beego.ControllerComments{
-			Method: "GetLogs",
-			Router: `/logs`,
+			Method:           "GetLogs",
+			Router:           `/logs`,
 			AllowHTTPMethods: []string{"get"},
-			MethodParams: param.Make(),
-			Params: nil})
+			MethodParams:     param.Make(),
+			Params:           nil})
 
 	beego.GlobalControllerRouter["metal/controllers:AdminController"] = append(beego.GlobalControllerRouter["metal/controllers:AdminController"],
 		beego.ControllerComments{
-			Method: "UploadImg",
-			Router: `/uploadImg`,
+			Method:           "UploadImg",
+			Router:           `/uploadImg`,
 			AllowHTTPMethods: []string{"post"},
-			MethodParams: param.Make(),
-			Params: nil})
+			MethodParams:     param.Make(),
+			Params:           nil})
 
 	beego.GlobalControllerRouter["metal/controllers:GroupController"] = append(beego.GlobalControllerRouter["metal/controllers:GroupController"],
 		beego.ControllerComments{
-			Method: "GetUserRoles",
-			Router: `/user-roles/:userId`,
+			Method:           "GetUserRoles",
+			Router:           `/user-roles/:userId`,
 			AllowHTTPMethods: []string{"get"},
-			MethodParams: param.Make(),
-			Params: nil})
+			MethodParams:     param.Make(),
+			Params:           nil})
 
 	beego.GlobalControllerRouter["metal/controllers:GroupController"] = append(beego.GlobalControllerRouter["metal/controllers:GroupController"],
 		beego.ControllerComments{
-			Method: "AddUserRole",
-			Router: `/user/groups`,
+			Method:           "AddUserRole",
+			Router:           `/user/groups`,
 			AllowHTTPMethods: []string{"post"},
-			MethodParams: param.Make(),
-			Params: nil})
+			MethodParams:     param.Make(),
+			Params:           nil})
 
 	beego.GlobalControllerRouter["metal/controllers:JobCountController"] = append(beego.GlobalControllerRouter["metal/controllers:JobCountController"],
 		beego.ControllerComments{
-			Method: "JobCount",
-			Router: `/job-count`,
+			Method:           "JobCount",
+			Router:           `/job-count`,
 			AllowHTTPMethods: []string{"get"},
-			MethodParams: param.Make(),
-			Params: nil})
+			MethodParams:     param.Make(),
+			Params:           nil})
 
 	beego.GlobalControllerRouter["metal/controllers:JobCountController"] = append(beego.GlobalControllerRouter["metal/controllers:JobCountController"],
 		beego.ControllerComments{
-			Method: "CountDataAll",
-			Router: `/job-count/count-data-all`,
+			Method:           "CountDataAll",
+			Router:           `/job-count/count-data-all`,
 			AllowHTTPMethods: []string{"get"},
-			MethodParams: param.Make(),
-			Params: nil})
+			MethodParams:     param.Make(),
+			Params:           nil})
 
 	beego.GlobalControllerRouter["metal/controllers:JobCountController"] = append(beego.GlobalControllerRouter["metal/controllers:JobCountController"],
 		beego.ControllerComments{
-			Method: "CountDataRecently",
-			Router: `/job-count/count-data-recently`,
+			Method:           "CountDataRecently",
+			Router:           `/job-count/count-data-recently`,
 			AllowHTTPMethods: []string{"get"},
-			MethodParams: param.Make(),
-			Params: nil})
+			MethodParams:     param.Make(),
+			Params:           nil})
 
 	beego.GlobalControllerRouter["metal/controllers:PortalController"] = append(beego.GlobalControllerRouter["metal/controllers:PortalController"],
 		beego.ControllerComments{
-			Method: "Get",
-			Router: `/`,
+			Method:           "Get",
+			Router:           `/`,
 			AllowHTTPMethods: []string{"get"},
-			MethodParams: param.Make(),
-			Params: nil})
+			MethodParams:     param.Make(),
+			Params:           nil})
 
 	beego.GlobalControllerRouter["metal/controllers:PortalController"] = append(beego.GlobalControllerRouter["metal/controllers:PortalController"],
 		beego.ControllerComments{
-			Method: "About",
-			Router: `/about`,
+			Method:           "About",
+			Router:           `/about`,
 			AllowHTTPMethods: []string{"get"},
-			MethodParams: param.Make(),
-			Params: nil})
+			MethodParams:     param.Make(),
+			Params:           nil})
 
 	beego.GlobalControllerRouter["metal/controllers:PortalController"] = append(beego.GlobalControllerRouter["metal/controllers:PortalController"],
 		beego.ControllerComments{
-			Method: "Article",
-			Router: `/article/:id`,
+			Method:           "Article",
+			Router:           `/article/:id`,
 			AllowHTTPMethods: []string{"get"},
-			MethodParams: param.Make(),
-			Params: nil})
+			MethodParams:     param.Make(),
+			Params:           nil})
 
 	beego.GlobalControllerRouter["metal/controllers:PortalController"] = append(beego.GlobalControllerRouter["metal/controllers:PortalController"],
 		beego.ControllerComments{
-			Method: "Category",
-			Router: `/category`,
+			Method:           "Category",
+			Router:           `/category`,
 			AllowHTTPMethods: []string{"get"},
-			MethodParams: param.Make(),
-			Params: nil})
+			MethodParams:     param.Make(),
+			Params:           nil})
 
 	beego.GlobalControllerRouter["metal/controllers:PortalController"] = append(beego.GlobalControllerRouter["metal/controllers:PortalController"],
 		beego.ControllerComments{
-			Method: "MyRoute",
-			Router: `/test`,
+			Method:           "MyRoute",
+			Router:           `/test`,
 			AllowHTTPMethods: []string{"get"},
-			MethodParams: param.Make(),
-			Params: nil})
+			MethodParams:     param.Make(),
+			Params:           nil})
 
 	beego.GlobalControllerRouter["metal/controllers:PortalController"] = append(beego.GlobalControllerRouter["metal/controllers:PortalController"],
 		beego.ControllerComments{
-			Method: "AddUser",
-			Router: `/user`,
+			Method:           "AddUser",
+			Router:           `/user`,
 			AllowHTTPMethods: []string{"post"},
-			MethodParams: param.Make(),
-			Params: nil})
+			MethodParams:     param.Make(),
+			Params:           nil})
 
 	beego.GlobalControllerRouter["metal/controllers:UserGroupController"] = append(beego.GlobalControllerRouter["metal/controllers:UserGroupController"],
 		beego.ControllerComments{
-			Method: "AddUserGroup",
-			Router: `/user-group/add-user-group`,
+			Method:           "AddUserGroup",
+			Router:           `/user-group/add-user-group`,
 			AllowHTTPMethods: []string{"post"},
-			MethodParams: param.Make(),
-			Params: nil})
+			MethodParams:     param.Make(),
+			Params:           nil})
 
 	beego.GlobalControllerRouter["metal/controllers:UserGroupController"] = append(beego.GlobalControllerRouter["metal/controllers:UserGroupController"],
 		beego.ControllerComments{
-			Method: "GetAllUserGroup",
-			Router: `/user-group/get-all-user-group`,
+			Method:           "GetAllUserGroup",
+			Router:           `/user-group/get-all-user-group`,
 			AllowHTTPMethods: []string{"get"},
-			MethodParams: param.Make(),
-			Params: nil})
+			MethodParams:     param.Make(),
+			Params:           nil})
 
 }
