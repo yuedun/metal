049087ac9c902e5facdd1ecfc8848b1b3a44b4c7
hale.huo <hale.huo@zhangmen.com>
2020-11-24 19:56:08 +0800

    用户启用禁用

--- controllers/permissions/permissions.go
+++ controllers/permissions/permissions.go
@@ -1,11 +1,10 @@
 package permissions
 
-/**
- * 此变量作用：
- * 需要验证权限的接口，如果值为false或没有配置代表不需要验证权限
- * 配置以后且值为true代表需要验证权限，并匹配数据库中用户是否存在对应权限
- * 可使用该方式集中维护权限，也可以由各个controller各自在Prepare维护自己权限，缺点是每个controller都要写一遍Prepare
- */
+// 此变量作用：
+// 需要验证权限的接口，如果值为false或没有配置代表不需要验证权限
+// 配置以后且值为true代表需要验证权限，并匹配数据库中用户是否存在对应权限
+// 可使用该方式集中维护权限，也可以由各个controller各自在Prepare维护自己权限，缺点是每个controller都要写一遍Prepare
+
 var NeedPermission = map[string]bool{
 	"AdminController:ToLogin":              false,
 	"AdminController:LoginOut":             false,
@@ -14,8 +13,9 @@ var NeedPermission = map[string]bool{
 	"AdminController:Welcome":              false,
 	"AdminController:UserList":             true,
 	"AdminController:UserListRoute":        true,
-	"AdminController:PUT":                  true,
-	"AdminController:POST":                 true,
+	"AdminController:UpdateUser":           true,
+	"AdminController:DisableUser":          true,
+	"AdminController:EnableUser":           true,
 	"AdminController:DeleteUser":           true,
 	"AdminController:ArticleEdit":          true,
 	"AdminController:ArticleDelete":        true,
@@ -32,11 +32,11 @@ var NeedPermission = map[string]bool{
 	"AdminController:TemplateView":         false,
 	"AdminController:TemplateList":         false,
 	"AdminController:UploadImg":            false,
-	"GroupController:GetUserRoles":         false,
-	"GroupController:AddUserRole":          true,
 	"JobCountController:JobCount":          false,
 	"JobCountController:CountDataAll":      false,
 	"JobCountController:CountDataRecently": false,
+	"UserGroupController:GetUserRoles":     false,
+	"UserGroupController:AddUserRole":      true,
 	"UserGroupController:AddUserGroup":     false,
 	"UserGroupController:GetAllUserGroup":  false,
 }
