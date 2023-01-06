28f7e52dabdbb2c6d87e546a054db7b0fcf1654d
yuedun <light_v@163.com>
2023-01-06 11:10:20 +0800

    优化404页面

--- controllers/base-admin.go
+++ controllers/base-admin.go
@@ -135,9 +135,9 @@ func (c *AdminBaseController) Prepare() {
 // 此变量作用：
 // 需要验证权限的接口，如果值为false或没有配置代表不需要验证权限
 // 配置以后且值为true代表需要验证权限，并匹配数据库中用户是否存在对应权限
-// 可使用该方式集中维护权限，也可以由各个controller各自在Prepare维护自己权限，缺点是每个controller都要写一遍Prepare
 var NeedPermission = map[string]bool{
 	//页面权限
+	"AdminPageController:Register":      false,
 	"AdminPageController:Welcome":       false,
 	"AdminPageController:RoleList":      false,
 	"AdminPageController:UserList":      true,
